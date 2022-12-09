fa8d2a94b570477fb852ec794c8b7da1635d18ce
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-09 14:24:27 +0530

    added controller

---
+++ src/main/java/com/nest/OnlineComplainReportingApplication/controller/ReportController.java
@@ -0,0 +1,7 @@
+package com.nest.OnlineComplainReportingApplication.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportController {
+}
