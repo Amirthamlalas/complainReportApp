1dc06d4c6b33051458deb8f26e806bd6a9e1f8da
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-09 14:20:42 +0530

    first commit

---
+++ src/main/java/com/nest/OnlineComplainReportingApplication/OnlineComplainReportingApplication.java
@@ -0,0 +1,13 @@
+package com.nest.OnlineComplainReportingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineComplainReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineComplainReportingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/OnlineComplainReportingApplication/OnlineComplainReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.OnlineComplainReportingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineComplainReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
