2c6bf541931ac7ddd7cac5adbf86f6a45d8c8787
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-09 15:02:25 +0530

    added userlogin

--- src/main/java/com/nest/OnlineComplainReportingApplication/controller/ReportController.java
+++ src/main/java/com/nest/OnlineComplainReportingApplication/controller/ReportController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
+
 @RestController
 public class ReportController {
 @Autowired
@@ -19,5 +22,23 @@ private UserDao dao;
         System.out.println(r.getName());
         dao.save(r);
         return "signup working";
+    }
+
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/userlogin",produces = "application/json",consumes = "application/json")
+    public HashMap<String, String> userLogin(@RequestBody Report r){
+        System.out.println(r.getName());
+        List<Report> result = (List<Report>) dao.UserLogin(r.getUsername(),r.getPassword());
+        HashMap<String,String>map=new HashMap<>();
+        if(result.size()==0){
+            map.put("status","failed");
+        }else {
+            int id = result.get(0).getId();
+            map.put("userid",String.valueOf(id));
+            map.put("status","success");
+        }
+        return map;
+
     }
 }
--- src/main/java/com/nest/OnlineComplainReportingApplication/dao/UserDao.java
+++ src/main/java/com/nest/OnlineComplainReportingApplication/dao/UserDao.java
@@ -1,9 +1,14 @@
 package com.nest.OnlineComplainReportingApplication.dao;
 
 import com.nest.OnlineComplainReportingApplication.model.Report;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserDao extends CrudRepository<Report, Integer> {
 
-
+    @Query(value = "SELECT * FROM `usersignup` WHERE `username`= :username AND `password`= :password",nativeQuery = true)
+    List<Report>UserLogin(@Param("username")String username,@Param("password")String password);
 }
